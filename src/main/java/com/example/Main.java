24843e75b67b397808dbdf4dea74678340bc166d
zezeina <zeina.shalaan@gmail.com>
2025-08-05 14:52:00 +0300

    ooo

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -3,5 +3,18 @@ package com.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int x ;
+        x = 5+5 ;
+        System.out.println("The value of x is: " + x);
+        int y = 10;
+        System.out.println("The value of y is: " + y);
+        int z = x + y;
+        System.out.println("The value of z (x + y) is: " + z);
+        if (z > 10) {
+            System.out.println("z is greater than 10");
+        } else {
+            System.out.println("z is not greater than 10");
+        }
+
     }
 }
